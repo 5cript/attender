85a259a93a3b812e030ce883c5ffaee964b6d481
5cript <Tim06TR@gmail.com>
2016-04-27 17:19:26 +0200

    Moved header parser out into its own class. Started work on reading functions for the body.

--- request.cpp
+++ request.cpp
@@ -1,55 +1,110 @@
 #include "request.hpp"
 
 #include "tcp_connection.hpp"
+#include "tcp_read_sink.hpp"
 
 #include <string>
-#include <boost/iostreams/stream.hpp>
 #include <iostream>
+#include <boost/lexical_cast.hpp>
 
 namespace attender
 {
 //#####################################################################################################################
     request_handler::request_handler(std::shared_ptr <tcp_connection> connection)
-        : connection_(std::move(connection))
+        : parser_{}
+        , connection_(std::move(connection))
+        , sink_(nullptr)
     {
+    }
+//---------------------------------------------------------------------------------------------------------------------
+    void request_handler::initiate_header_read(parse_callback on_parse)
+    {
+        on_parse_ = on_parse;
         connection_->set_read_callback([this](boost::system::error_code ec) {
             header_read_handler(ec);
         });
         connection_->read();
     }
 //---------------------------------------------------------------------------------------------------------------------
+    request_header request_handler::get_header() const
+    {
+        return parser_.get_header();
+    }
+//---------------------------------------------------------------------------------------------------------------------
     void request_handler::header_read_handler(boost::system::error_code ec)
     {
         if (ec)
+            on_parse_(ec);
+
+        auto* ptr = connection_.get();
+        parser_.feed(connection_.get());
+
+        if (parser_.finished())
+            on_parse_({});
+    }
+//---------------------------------------------------------------------------------------------------------------------
+    readable_request_handler request_handler::initiate_read(read_callback callback)
+    {
+        return readable_request_handler(this, std::move(callback)); // DO NOT BREAK HERE!!!!
+    }
+//---------------------------------------------------------------------------------------------------------------------
+    uint64_t request_handler::get_content_length() const
+    {
+        // TODO: use memoization.
+
+        auto header = parser_.get_header();
+        auto body_length = header.fields.find("Content-Length");
+
+        if (body_length == header.fields.end())
+            throw std::runtime_error("no content length field provided for reading body");
+
+        // throws if Content-Length is not a anumber
+        return boost::lexical_cast <uint64_t> (body_length->second);
+    }
+//#####################################################################################################################
+    readable_request_handler::readable_request_handler(request_handler* parent, read_callback callback)
+        : res_(parent)
+        , callback_(std::move(callback))
+    {
+        res_->connection_->set_read_callback([this](boost::system::error_code ec) {
+            body_read_handler(ec);
+        });
+    }
+//---------------------------------------------------------------------------------------------------------------------
+    void readable_request_handler::body_read_handler(boost::system::error_code ec)
+    {
+        if (ec)
         {
-            std::cerr << ec.message() << "\n";
+            std::cerr << "-";
         }
-        std::cerr << "read something!\n";
 
-        tcp_stream_device device {connection_.get()};
-        boost::iostreams::stream <tcp_stream_device> stream(device);
+        res_->sink_->write(res_->connection_->get_read_buffer());
 
-        do
-        {
-            std::string line;
-            auto hit_eof = std::getline(stream, line, '\n').eof();
-            header_buffer_ += line;
-            if (line.back() == '\r') {
-                header_buffer_ += "\r\n";
-                line.pop_back();
-            }
+        auto remaining = res_->get_content_length() - res_->sink_->get_bytes_written();
 
-            if (!hit_eof && line.empty())
-                break;
-            else if (hit_eof) {
-                connection_->read();
-                break;
-            }
-        } while(true);
+        if (remaining == 0)
+            callback_({});
+        else
+            res_->connection_->read();
+    }
+//---------------------------------------------------------------------------------------------------------------------
+    void readable_request_handler::read_body(std::ostream& stream)
+    {
+        // assign a sink, which prevails the asynchronous structure.
+        res_->sink_.reset(new tcp_stream_sink(&stream));
 
-        std::cout << header_buffer_ << "\n";
+        // write what we already have read by parsing the header
+        auto body_begin = res_->parser_.get_buffer(); // start of body
+        res_->sink_->write(body_begin.c_str(), body_begin.size());
 
-        connection_->write("HTTP/1.1 200 OK");
+        std::cout << res_->get_content_length() << "\n";
+        std::cout << body_begin.size() << "\n";
+
+        // read more if more data is to be expected.
+        if (res_->get_content_length() - body_begin.size() > 0)
+            res_->connection_->read();
+        else
+            callback_({});
     }
 //#####################################################################################################################
 }
--- request.hpp
+++ request.hpp
@@ -2,21 +2,59 @@
 
 #include "tcp_fwd.hpp"
 #include "request_header.hpp"
+#include "request_parser.hpp"
+
+#include <iosfwd>
 
 namespace attender
 {
+    class readable_request_handler;
+
+//#####################################################################################################################
     class request_handler
     {
+        friend readable_request_handler;
+
     public:
         request_handler(std::shared_ptr <tcp_connection> connection);
+
+        /**
+         *  Returns the request header.
+         *
+         *  @return A http request header.
+         */
+        request_header get_header() const;
+
+        void initiate_header_read(parse_callback on_parse);
+
+        readable_request_handler initiate_read(read_callback callback);
 
     private:
         // read handlers
         void header_read_handler(boost::system::error_code ec);
+        uint64_t get_content_length() const;
 
     private:
-        std::string header_buffer_;
-        request_header header_;
+        request_parser parser_;
         std::shared_ptr <tcp_connection> connection_;
+        std::shared_ptr <tcp_read_sink> sink_;
+        parse_callback on_parse_;
     };
+//#####################################################################################################################
+    class readable_request_handler
+    {
+        friend request_handler;
+
+    public:
+        void read_body(std::ostream& stream);
+
+    private:
+        readable_request_handler(request_handler* parent, read_callback callback);
+        void body_read_handler(boost::system::error_code ec);
+
+    private:
+        request_handler* res_;
+        read_callback callback_;
+    };
+//#####################################################################################################################
 }
