dc90113b6cfccc8982e2604d99f06362c16370e6
5cript <Tim06TR@gmail.com>
2016-04-27 17:17:10 +0200

    Added macros to make the configuration of constant limits possible on compilation.

--- net_core.hpp
+++ net_core.hpp
@@ -3,12 +3,29 @@
 #include <cstddef>
 #include <boost/asio.hpp>
 
+// these macros are meant to be overridden by -D flag
+// do not define them anywhere else.
+
+#ifndef CONFIG_MAX_HEADER_FIELDS
+#   define CONFIG_MAX_HEADER_FIELDS 1024
+#endif // CONFIG_MAX_HEADER_FIELDS
+
+#ifndef CONFIG_MAX_HEADER_BUFFER
+#   define CONFIG_MAX_HEADER_BUFFER 16384
+#endif // CONFIG_MAX_HEADER_BUFFER
+
+#ifndef CONFIG_RECEIVE_BUFFER_SIZE
+#   define CONFIG_RECEIVE_BUFFER_SIZE 4096
+#endif // CONFIG_RECEIVE_BUFFER_SIZE
+
 namespace attender
 {
     namespace asio = boost::asio;
 
     namespace config
     {
-        constexpr static std::size_t buffer_size = 10;
+        constexpr static std::size_t buffer_size = CONFIG_RECEIVE_BUFFER_SIZE;
+        constexpr static std::size_t header_buffer_max = CONFIG_MAX_HEADER_BUFFER;
+        constexpr static std::size_t header_field_max = CONFIG_MAX_HEADER_FIELDS;
     }
 }
