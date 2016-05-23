dc68abfcc7d1604f211eaf0b09861061ffb86278
5cript <Tim06TR@gmail.com>
2016-05-23 16:42:16 +0200

    Implemented res.set and res.append.

--- response.cpp
+++ response.cpp
@@ -2,9 +2,22 @@
 
 namespace attender
 {
+
+//#####################################################################################################################
     response_handler::response_handler(std::shared_ptr <tcp_connection> connection)
         : connection_(connection)
     {
 
     }
+//---------------------------------------------------------------------------------------------------------------------
+    void response_handler::append(std::string const& field, std::string const& value)
+    {
+        header_.append_field(field, value);
+    }
+//---------------------------------------------------------------------------------------------------------------------
+    response_handler& response_handler::set(std::string const& field, std::string const& value)
+    {
+        header_.set_field(field, value);
+    }
+//#####################################################################################################################
 }
--- response.hpp
+++ response.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "tcp_fwd.hpp"
+#include "response_header.hpp"
 
 namespace attender
 {
@@ -9,7 +10,34 @@ namespace attender
     public:
         response_handler(std::shared_ptr <tcp_connection> connection);
 
+        /**
+         *  Appends the specified value to the HTTP response header field.
+         *  If the header is not already set, it creates the header with the specified value.
+         *
+         *  @param field A header field identifier, such as "Warning" or "Link"
+         *  @param value A value that is to be appended.
+         *
+         *  @return *this for chaining.
+         */
+        response_handler& append(std::string const& field, std::string const& value);
+
+        /**
+         *  Sets the response’s HTTP header field to value.
+         *
+         *  @param field A header field identifier, such as "Warning" or "Link"
+         *  @param value A value that is to be appended.
+         *
+         *  @return *this for chaining.
+         */
+        response_handler& set(std::string const& field, std::string const& value);
+
+        // TODO:
+        // cookie, attachement, download, format(does not belong here), json, xml
+
+
+
     private:
         std::shared_ptr <tcp_connection> connection_;
+        response_header header_;
     };
 }
