e2d937e5d13e586569af480031629e0156305f28
5cript <Tim06TR@gmail.com>
2016-05-23 19:32:37 +0200

    Added debug destructor, started work on send.

--- response.cpp
+++ response.cpp
@@ -1,13 +1,49 @@
 #include "response.hpp"
+#include "tcp_connection.hpp"
+#include "tcp_server.hpp"
+
+#include <iostream>
 
 namespace attender
 {
+//#####################################################################################################################
+    /**
+     *  Trick to hide template in c++ file. So that tcp_connection.hpp is not required in the header.
+     */
+    template <typename T>
+    void write(response_handler* repHandler, std::shared_ptr <T> data)
+    {
+        repHandler->send_header([repHandler, data](boost::system::error_code ec){
+            // end the connection, on error
+            if (ec)
+            {
+                repHandler->end();
+                return;
+            }
 
+            repHandler->get_connection()->write(*data, [repHandler](boost::system::error_code ec){
+                // end no matter what.
+                repHandler->end();
+            });
+        });
+    }
 //#####################################################################################################################
     response_handler::response_handler(std::shared_ptr <tcp_connection> connection)
-        : connection_(connection)
+        : connection_{connection}
+        , header_{}
+        , headerSent_{false}
     {
 
+    }
+//---------------------------------------------------------------------------------------------------------------------
+    response_handler::~response_handler()
+    {
+        std::cout << "response destroyed\n";
+    }
+//---------------------------------------------------------------------------------------------------------------------
+    tcp_connection_interface* response_handler::get_connection()
+    {
+        return connection_.get();
     }
 //---------------------------------------------------------------------------------------------------------------------
     response_handler&  response_handler::append(std::string const& field, std::string const& value)
@@ -20,6 +56,55 @@ namespace attender
     {
         header_.set_field(field, value);
         return *this;
+    }
+//---------------------------------------------------------------------------------------------------------------------
+    void response_handler::send(std::string const& body)
+    {
+        try_set("Content-Length", std::to_string(body.length()));
+        try_set("Content-Type", "text/plain");
+
+        write(this, std::make_shared <std::string> (body));
+    }
+//---------------------------------------------------------------------------------------------------------------------
+    void response_handler::send(std::vector <char> const& body)
+    {
+        try_set("Content-Length", std::to_string(body.size()));
+        try_set("Content-Type", "application/octet-stream");
+
+        write(this, std::make_shared <std::vector <char>> (body));
+    }
+//---------------------------------------------------------------------------------------------------------------------
+    void response_handler::end()
+    {
+        send_header([this](boost::system::error_code ec){
+            connection_->shutdown();
+            connection_->get_parent()->get_connections()->remove(connection_);
+        });
+    }
+//---------------------------------------------------------------------------------------------------------------------
+    void response_handler::try_set(std::string const& field, std::string const& value)
+    {
+        if (!header_.has_field(field))
+            set(field, value);
+    }
+//---------------------------------------------------------------------------------------------------------------------
+    response_handler& response_handler::status(int code)
+    {
+        header_.set_code(code);
+        return *this;
+    }
+//---------------------------------------------------------------------------------------------------------------------
+    void response_handler::send_header(write_callback continuation)
+    {
+        if (headerSent_.load() == false)
+        {
+            headerSent_.store(true);
+            connection_->write(header_.to_string(), continuation);
+        }
+        else
+        {
+            continuation({});
+        }
     }
 //#####################################################################################################################
 }
--- response.hpp
+++ response.hpp
@@ -3,12 +3,18 @@
 #include "tcp_fwd.hpp"
 #include "response_header.hpp"
 
+#include <atomic>
+
 namespace attender
 {
     class response_handler
     {
     public:
+        using shared_connection = std::shared_ptr <tcp_connection_interface>;
+
+    public:
         response_handler(std::shared_ptr <tcp_connection> connection);
+        ~response_handler();
 
         /**
          *  Appends the specified value to the HTTP response header field.
@@ -31,13 +37,71 @@ namespace attender
          */
         response_handler& set(std::string const& field, std::string const& value);
 
+        /**
+         *  Sets the response code. code must be valid/known.
+         *
+         *  @param code A response code 1xx, 2xx, 3xx, 4xx or 5xx
+         *
+         *  @return *this for chaining.
+         */
+        response_handler& status(int code);
+
+        /**
+         *  Sends the HTTP response. After a call to send, the status and header fields
+         *  can no longer be changed as they will be sent with this function.
+         *  As this function completes the response, chaining will no longer be possible.
+         *
+         *  Content-Length will automatically be set, if not previously defined.
+         *  Content-Type will be set to "text/plain" for this overload.
+         *
+         *  @param body A body to send.
+         */
+        void send(std::string const& body);
+
+        /**
+         *  Sends the HTTP response. After a call to send, the status and header fields
+         *  can no longer be changed as they will be sent with this function.
+         *  As this function completes the response, chaining will no longer be possible.
+         *
+         *  Content-Length will automatically be set, if not previously defined.
+         *  Content-Type will be set to "application/octet-stream" for this overload.
+         *
+         *  @param body A body to send.
+         */
+        void send(std::vector <char> const& body);
+
+        /**
+         *  Ends the response process.
+         *  Use to quickly end the response without any data. If you need to respond with data,
+         *  instead use methods such as send and json.
+         *  As this function completes the response, chaining will no longer be possible.
+         */
+        void end();
+
+        /**
+         *  Do NOT use this function!
+         *  Returns a handle to the underlying connection.
+         */
+        tcp_connection_interface* get_connection();
+
+        /**
+         *  Do NOT use this function!
+         *  Will send the header and set the "headerSent_" flag.
+         */
+        void send_header(write_callback continuation);
+
         // TODO:
         // cookie, attachement, download, format(does not belong here), json, xml
 
-
+    private:
+        /**
+         *  Like set, but only sets the field, if not provided earlier.
+         */
+        void try_set(std::string const& field, std::string const& value);
 
     private:
-        std::shared_ptr <tcp_connection> connection_;
+        shared_connection connection_;
         response_header header_;
+        std::atomic_bool headerSent_;
     };
 }
