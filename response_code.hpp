8cc1da1a3aae688b9354d6a92f45abc73406ca82
5cript <Tim06TR@gmail.com>
2016-05-23 19:31:15 +0200

    Added mapping for response codes to string representation.

---
+++ response_code.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+namespace attender
+{
+    /**
+     *  Returns a string representation for a given code number.
+     */
+    std::string translate_code(int status);
+}
