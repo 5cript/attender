aeeae643a0ae90b8e8dfbe7dc6b9db0dbd8a1792
5cript <Tim06TR@gmail.com>
2016-05-23 19:33:32 +0200

    Added methods to interface.

--- tcp_connection_interface.hpp
+++ tcp_connection_interface.hpp
@@ -1,10 +1,21 @@
 #pragma once
 
+#include "tcp_fwd.hpp"
+
+#include <iosfwd>
+#include <string>
+#include <vector>
+
 namespace attender
 {
     struct tcp_connection_interface
     {
         virtual void start() = 0;
         virtual void stop() = 0;
+        virtual void shutdown() = 0;
+        virtual void write(std::istream& stream, write_callback handler) = 0;
+        virtual void write(std::string const& string, write_callback handler) = 0;
+        virtual void write(std::vector <char> const& container, write_callback handler) = 0;
+        virtual tcp_server_interface* get_parent() = 0;
     };
 }
