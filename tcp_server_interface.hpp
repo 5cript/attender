755fc0c4894ff5162732f098fa398616b8933ddd
5cript <Tim06TR@gmail.com>
2016-05-23 19:34:01 +0200

    Added methods to interface.

--- tcp_server_interface.hpp
+++ tcp_server_interface.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "settings.hpp"
+#include "tcp_fwd.hpp"
 
 #include <string>
 
@@ -13,5 +14,6 @@ namespace attender
         virtual void start(std::string const& port, std::string const& host) = 0;
         virtual void stop() = 0;
         virtual settings get_settings() const = 0;
+        virtual connection_manager* get_connections() = 0;
     };
 }
