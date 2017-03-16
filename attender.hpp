d7752fd09f6ef5a8eab887b85a886efda57cca2c
5cript <Tim06TR@gmail.com>
2017-03-16 08:43:45 +0100

    Move to subfolder.

--- attender.hpp
+++ attender.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
-#include "tcp_server.hpp"
-#include "tcp_secure_server.hpp"
-#include "tcp_connection.hpp"
+#include "attender/tcp_server.hpp"
+#include "attender/tcp_secure_server.hpp"
+#include "attender/tcp_connection.hpp"
 
-#include "io_context/managed_io_context.hpp"
-#include "io_context/thread_pooler.hpp"
+#include "attender/io_context/managed_io_context.hpp"
+#include "attender/io_context/thread_pooler.hpp"
 
-#include "response.hpp"
-#include "request.hpp"
+#include "attender/ssl_contexts/ssl_example_context.hpp"
+
+#include "attender/response.hpp"
+#include "attender/request.hpp"
