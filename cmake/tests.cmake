615f7101f8f649b0e230a78ed4438908a81815a4
5cript <tim06tr@gmail.com>
2021-04-25 17:33:46 +0200

    Fixed building.

--- cmake/tests.cmake
+++
@@ -1,6 +0,0 @@
-# Version check
-cmake_minimum_required (VERSION 3.20)
-
-add_executable(test-attender ${sources} "${CMAKE_CURRENT_SOURCE_DIR}/test/tests.cpp")
-
-target_link_libraries(test-attender PRIVATE attender -lgtest)
