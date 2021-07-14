fc0096158a4e95b3c2a8ae834c070a787bd7f4d2
Tim Ebbeke <tim06tr@gmail.com>
2021-07-14 21:55:40 +0200

    Fixed includes.

--- attender.hpp
+++ attender.hpp
@@ -1,17 +1,17 @@
 #pragma once
 
-#include "attender/http_server.hpp"
-#include "attender/http_secure_server.hpp"
-#include "attender/http_connection.hpp"
+#include <attender/http/http_server.hpp>
+#include <attender/http/http_secure_server.hpp>
+#include <attender/http/http_connection.hpp>
 
-#include "attender/io_context/managed_io_context.hpp"
-#include "attender/io_context/thread_pooler.hpp"
+#include <attender/io_context/managed_io_context.hpp>
+#include <attender/io_context/thread_pooler.hpp>
 
-#include "attender/ssl_contexts/ssl_example_context.hpp"
+#include <attender/ssl_contexts/ssl_example_context.hpp>
 
-#include "attender/response.hpp"
-#include "attender/request.hpp"
+#include <attender/http/response.hpp>
+#include <attender/http/request.hpp>
 
 // Encoders
-#include "attender/encoding/streaming_producer.hpp"
-#include "attender/encoding/brotli.hpp"
+#include <attender/encoding/streaming_producer.hpp>
+#include <attender/encoding/brotli.hpp>
